dd9a49cd10822bf2af457cead1fa1568ddf55a87
Gözde Apak <gozdeapak.ga.1@gmail.com>
2024-03-21 14:31:02 +0300

    login ve register buttonları anasayfaya eklendi

--- client/src/App.js
+++ client/src/App.js
@@ -3,14 +3,27 @@ import { Box, Container } from '@mui/material';
 import './App.css';
 import TodoList from './components/TodoList';
 
+import {
+  createBrowserRouter,
+  Link,
+  RouterProvider,
+} from "react-router-dom";
+import { Button } from 'flowbite-react';
+
 function App() {
   return (
- <Container fixed sx={{justifyContent : "center", display:"flex"}}>
+ <Container fixed sx={{justifyContent : "center", display:"flex", flexDirection:"column"}}>
+
+   <div style={{display : 'flex' , justifyContent:"flex-end", marginTop:"2rem"}}>
+   <Link to="/login"><Button style={{width:"100px", height:"45px", marginRight:"20px", borderRadius : "22px"}}>Login</Button></Link>
+  <Link to="/register"><Button style={{width:"100px", height:"45px", borderRadius : "22px"}}>Register</Button></Link>
+   </div>
+
      <Box 
-    width={685}
+    
     display="flex"
     flexDirection={'column'}
-    justifyContent={'start'}
+    justifyContent={'center'}
     bgcolor={'white'}
     textAlign={'center'}
     margin={10} 
